e70c8abc84ac823d823ddd1d1831e2d10ca6c323
ashwin <ashu61998@gmail.com>
2021-11-18 22:16:19 +0530

    image edited and database added

--- js/components/TheLightboxComponent.js
+++ js/components/TheLightboxComponent.js
@@ -3,18 +3,13 @@ import TheVideoComponent from './TheVideoComponent.js';
 export default {
     name: "TheLightboxComponent",
     props: ["model"], 
-     computed: {
-        activeComponent: function(){
-            return `${this.model.mediaType + "Component"}`;
-        }
-    },
     template:  `<section class="lightboxWrapper">
     <section class="lightboxContent">
         <h1>{{ model.mini_name }}</h1>
         <p>{{ model.mini_description }}</p>
         </section>
         <section class="videoBox">
-        <component v-if="model.mediaType" :is="activeComponent"></component>
+        <VideoComponent></VideoComponent>
         </section>
         </section>`,
     methods: {
