bcbb8e00aad001aa2411d4b53d8ef3c8cce22d01
ashwin <ashu61998@gmail.com>
2021-11-24 23:32:04 +0530

    close lightbox option fixed, volume modified

--- js/components/TheVideoComponent.js
+++ js/components/TheVideoComponent.js
@@ -2,6 +2,10 @@ export default {
     name: "VideoComponent",
     props: ["model"],
     template : `<section class="video-player">
-   <video :src='"images/" + model.mini_video'></video>
-</section>`
+   <video :src='"images/" + model.mini_video' autoplay></video>
+</section>`,
+
+    mounted: function(){
+document.querySelector("video").volume = 0.1;
+    },
 }
